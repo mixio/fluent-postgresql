4a6ef681c9a62626e6fb8e3288563b6c93930099
Jean Jourdain <jean.jourdain@mixio.com>
2018-09-24 18:50:48 +0200

    User mixio modules in package

--- Package.swift
+++ Package.swift
@@ -9,13 +9,16 @@ let package = Package(
     ],
     dependencies: [
         // 🌎 Utility package containing tools for byte manipulation, Codable, OS APIs, and debugging.
-        .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
+//      .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
+        .package(url: "https://github.com/mixio/core.git", .branch("mixio-dev")),
 
         // Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
+//      .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
+        .package(url: "https://github.com/mixio/fluent.git", .branch("mixio-dev")),
 
         // 🐘 Non-blocking, event-driven Swift client for PostgreSQL.
-        .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0"),
+//      .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/mixio/postgresql.git", .branch("mixio-dev")),
     ],
     targets: [
         .target(name: "FluentPostgreSQL", dependencies: ["Async", "FluentSQL", "PostgreSQL"]),
